d7023c986043faf08506232e0a8cc032769de31d
ACER <danyalzatearias2016@gmail.com>
2023-08-01 11:29:46 -0500

    Innitial commit

--- Exercise_repaso/exercise2.py
+++ Exercise_repaso/exercise2.py
@@ -1,2 +1,9 @@
 #Escribir una función que calcule el área de un círculo dado su radio.
 
+from math import pi
+
+rad = float(input("Ingrese un numero"))
+
+area_circulo = pi * (rad) **2
+
+print(area_circulo)
--- Exercise_repaso/exercise4.py
+++ Exercise_repaso/exercise4.py
@@ -1,4 +1,4 @@
-numero = int(input("Ingresa un número entero: "))
+numero = float(input("Ingresa un número entero: "))
 
 if numero % 2 == 0: #el % funciona como divisor y el 2 como dividendo, el numero ingresado se divide por 2 y el programa muestra automaticamente si es par o impar
     print("El número es par.")
