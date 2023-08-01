4fd4b302b4531f1a75f1bd8be22e07a9979898b7
ACER <danyalzatearias2016@gmail.com>
2023-08-01 11:38:41 -0500

    Innitial commit

--- Exercise_repaso/exercise3.py
+++ Exercise_repaso/exercise3.py
@@ -1,1 +1,12 @@
 #Crear un programa que genere una lista de números aleatorios y los imprima en pantalla.
+import random
+
+cantidad_numeros = 5
+
+numeros_aleatorios = []
+for _ in range(cantidad_numeros):
+    numero = random.randint(1, 100)  # Genera un número aleatorio entre 1 y 100
+    numeros_aleatorios.append(numero)
+
+print(numeros_aleatorios)
+
