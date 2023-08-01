7fa4f5bcb724d03f3e3647f9388a22073f94a11a
ACER <danyalzatearias2016@gmail.com>
2023-08-01 11:16:01 -0500

    Innitial commit

---
+++ Exercise_repaso/exercise4.py
@@ -0,0 +1,6 @@
+numero = int(input("Ingresa un número entero: "))
+
+if numero % 2 == 0: #el % funciona como divisor y el 2 como dividendo, el numero ingresado se divide por 2 y el programa muestra automaticamente si es par o impar
+    print("El número es par.")
+else:
+    print("El número es impar.")
