967ff43e48b3f488d5725647bbae865f7690c0fc
ACER <danyalzatearias2016@gmail.com>
2023-08-08 10:23:11 -0500

    Innitial commit

---
+++ participacion_3/ejercicio_1.py
@@ -0,0 +1,6 @@
+#Cree una clase Vehículo que contenga los atributos de instancia velocidad_maxima y kilometraje.
+class vehiculo:
+    def __init__(self, velocidad_maxima: float, kilometraje: float):
+        self.velocidad_maxima = float = velocidad_maxima
+        self.kilometraje = float = kilometraje
+
